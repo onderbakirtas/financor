5d97e2c0c8886a28fc8fb2f2a6fc253182ce8abe
Onder Bakirtas <onderbakirtas@gmail.com>
2021-04-03 06:02:49 +0300

    feat: project init

---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<main>
+	<h1>Hello world!</h1>
+
+	<Counter />
+
+	<p>Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style style lang="postcss">
+	main {
+		@apply text-center;
+		@apply p-4;
+		@apply mx-auto;
+	}
+
+	h1 {
+		@apply text-red-600;
+		@apply uppercase;
+		@apply text-6xl;
+		@apply font-thin;
+		@apply leading-tight;
+		@apply my-16 mx-auto;
+		@apply max-w-xs;
+	}
+
+	p {
+		@apply max-w-xs;
+		@apply my-8 mx-auto;
+		@apply leading-snug;
+	}
+
+	@screen sm {
+		h1 {
+			@apply max-w-none;
+		}
+
+		p {
+			@apply max-w-none;
+		}
+	}
+</style>
