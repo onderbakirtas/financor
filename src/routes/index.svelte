ad6d12dc6a07244b2f1aa56912b4ed381f475d9d
Onder Bakirtas <onderbakirtas@gmail.com>
2021-04-04 23:51:45 +0300

    feat: add features section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Hero from '$lib/Hero.svelte';
+	import Features from '$lib/Features.svelte';
 </script>
 
 <main>
 	<Hero />
+	<Features />
 </main>
 
 <style style lang="postcss">
--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
 const node = require('@sveltejs/adapter-node');
-const vercel = require('@sveltejs/adapter-vercel')
+// const vercel = require('@sveltejs/adapter-vercel')
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -19,7 +19,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: vercel(),
+		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
