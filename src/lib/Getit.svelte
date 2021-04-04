87953fb112474a11ae304cf19f50670b58571c36
Onder Bakirtas <onderbakirtas@gmail.com>
2021-04-05 01:13:51 +0300

    fix: styled getit body

--- src/lib/Getit.svelte
+++ src/lib/Getit.svelte
@@ -1,9 +1,32 @@
 <section class="getit">
-
+	<main class="getit-body">
+		<h3 class="getit-title">Download <span>Financor</span> App</h3>
+		<article class="getit-content">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate provident nulla similique,
+			voluptates id harum deserunt corrupti ea officia sed totam ullam ex dicta tempora? Quas
+			maiores consectetur obcaecati nam. Cupiditate provident nulla similique, voluptates id harum
+			deserunt corrupti ea officia sed totam ullam ex dicta tempora? Quas maiores consectetur
+			obcaecati nam.
+		</article>
+	</main>
+	<aside class="getit-phones" />
 </section>
 
 <style lang="postcss">
-.getit {
-  @apply bg-purple-600;
-}
+	.getit {
+		@apply bg-purple-700 rounded-3xl max-w-screen-xl mx-auto mt-32 mb-40;
+	}
+
+  .getit-body {
+    @apply px-12 py-20 w-3/5;
+  }
+
+  .getit-title {
+    @apply text-4xl font-bold text-white;
+		font-family: 'Jost', sans-serif;
+  }
+
+  .getit-content {
+    @apply mt-3 text-lg text-purple-200;
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,14 @@
 	import Hero from '$lib/Hero.svelte';
 	import Features from '$lib/Features.svelte';
 	import Advantages from '$lib/Advantages.svelte';
+	import Getit from '$lib/Getit.svelte';
 </script>
 
 <main>
 	<Hero />
 	<Features />
 	<Advantages />
+	<Getit />
 </main>
 
 <style style lang="postcss">
