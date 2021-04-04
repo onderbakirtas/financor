c35fdbf7d4f5dbb6c0beb73be5c06d236eb56acc
Onder Bakirtas <onderbakirtas@gmail.com>
2021-04-04 03:42:24 +0300

    feat: landing page hero

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+import Logo from "./Logo.svelte";
+import NavbarCta from "./NavbarCta.svelte";
+import NavbarMenu from "./NavbarMenu.svelte";
+
+</script>
+<nav class="navbar">
+  <Logo />
+  <NavbarMenu />
+  <NavbarCta />
+</nav>
+
+<style lang="postcss">
+.navbar {
+  @apply py-3 px-6 flex justify-between items-center fixed w-full z-50;
+  font-family: 'Jost', sans-serif;
+}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,13 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import Hero from '$lib/Hero.svelte';
 </script>
 
 <main>
-	<h1>Hello world!</h1>
-
-	<Counter />
-
-	<p>Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+	<Hero />
 </main>
 
 <style style lang="postcss">
 	main {
-		@apply text-center;
-		@apply p-4;
 		@apply mx-auto;
-	}
-
-	h1 {
-		@apply text-red-600;
-		@apply uppercase;
-		@apply text-6xl;
-		@apply font-thin;
-		@apply leading-tight;
-		@apply my-16 mx-auto;
-		@apply max-w-xs;
-	}
-
-	p {
-		@apply max-w-xs;
-		@apply my-8 mx-auto;
-		@apply leading-snug;
-	}
-
-	@screen sm {
-		h1 {
-			@apply max-w-none;
-		}
-
-		p {
-			@apply max-w-none;
-		}
 	}
 </style>
