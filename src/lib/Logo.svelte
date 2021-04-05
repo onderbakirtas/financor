953a0e73a73016d6ddab19d9dfed6c2f738c0f97
Onder Bakirtas <onderbakirtas@gmail.com>
2021-04-06 01:56:52 +0300

    fix: navbar change style on scroll
    Signed-off-by: Onder Bakirtas <onderbakirtas@gmail.com>

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,20 +1,26 @@
+<script lang="ts">
+	export let inverted = true;
+
+	$: logoPath = inverted ? '../img/logo-p.png' : '../img/logo-w.png';
+</script>
+
 <a class="logo" href="/">
-  <img class="logo-img" src="../img/logo-w.png" alt="Logo">
-  <span class="logo-txt">Financor</span>
+	<img class="logo-img" src={logoPath} alt="Logo" />
+	<span class="logo-txt">Financor</span>
 </a>
 
 <style lang="postcss">
-.logo {
-  @apply inline-flex text-2xl items-center;
-}
+	.logo {
+		@apply inline-flex text-2xl items-center;
+	}
 
-.logo-img {
-  @apply h-10 inline-block;
-}
+	.logo-img {
+		@apply h-10 inline-block;
+	}
 
-.logo-txt {
-  @apply ml-2 font-bold text-white;
-  font-family: 'Jost', sans-serif;
-  font-weight: 700;
-}
+	.logo-txt {
+		@apply ml-2 font-bold text-white;
+		font-family: 'Jost', sans-serif;
+		font-weight: 700;
+	}
 </style>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,18 +1,49 @@
-<script>
-import Logo from "./Logo.svelte";
-import NavbarCta from "./NavbarCta.svelte";
-import NavbarMenu from "./NavbarMenu.svelte";
+<script lang="ts">
+	import { onMount } from 'svelte';
 
+	import Logo from './Logo.svelte';
+	import NavbarCta from './NavbarCta.svelte';
+	import NavbarMenu from './NavbarMenu.svelte';
+
+	let y = 0;
 </script>
-<nav class="navbar">
-  <Logo />
-  <NavbarMenu />
-  <NavbarCta />
+
+<svelte:window bind:scrollY={y} />
+
+<nav class="navbar" class:scrolled={y > 100}>
+	<Logo inverted={y > 100} />
+	<NavbarMenu/>
+	<NavbarCta />
 </nav>
 
 <style lang="postcss">
-.navbar {
-  @apply py-3 px-6 flex justify-between items-center fixed w-full z-50;
-  font-family: 'Jost', sans-serif;
-}
+	.navbar {
+		@apply py-3 px-6 flex justify-between items-center fixed w-full z-50 duration-150;
+    @apply border-b border-transparent;
+		font-family: 'Jost', sans-serif;
+	}
+
+  .navbar.scrolled {
+    @apply bg-white bg-opacity-90 border-gray-200;
+    backdrop-filter: blur(10px);
+  }
+
+  .navbar.scrolled :global(.logo-txt) {
+    @apply text-gray-700;
+  }
+
+  
+  .navbar.scrolled :global(.menu-item) {
+    @apply text-gray-800;
+  }
+
+  
+  .navbar.scrolled :global(.menu-item:hover) {
+    @apply text-gray-500;
+  }
+
+  .navbar.scrolled :global(.navbar-cta) {
+    @apply bg-purple-700 text-white;
+
+  }
 </style>
