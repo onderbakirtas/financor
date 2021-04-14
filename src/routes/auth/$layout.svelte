b31fb5368e146acb49d2142bb3c3ed964991f85d
Onder Bakirtas <onderbakirtas@gmail.com>
2021-04-14 23:29:25 +0300

    feat: add auth namespace

---
+++ src/routes/auth/$layout.svelte
@@ -0,0 +1,11 @@
+<div class="auth-layout">
+  <slot />
+</div>
+
+<style lang="postcss">
+
+.auth-layout {
+  @apply h-screen w-full flex items-center justify-center pb-32 bg-gray-200;
+}
+
+</style>
