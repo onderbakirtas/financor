8ec4087bdc865ffd0b7d82c8b4c5b15c1ad0a0dc
Onder Bakirtas <onderbakirtas@gmail.com>
2023-01-22 03:55:11 +0300

    style: remove redundant spaces in  file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import mkcert from'vite-plugin-mkcert'
-
+import mkcert from 'vite-plugin-mkcert';
 
 const config: UserConfig = {
 	plugins: [sveltekit(), mkcert()],
 	server: {
-		https: true,
+		https: true
 	}
 };
 
