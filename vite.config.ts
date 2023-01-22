f8023becfc4a625cfa63f531f4ac6ce8633a256f
Onder Bakirtas <onderbakirtas@gmail.com>
2023-01-22 03:52:15 +0300

    feat: add mkcert for https local dev env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import mkcert from'vite-plugin-mkcert'
+
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), mkcert()],
+	server: {
+		https: true,
+	}
 };
 
 export default config;
